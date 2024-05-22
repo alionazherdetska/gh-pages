f23b09e64cebe8234073bfcbcd55417d5218cf19
alona <alionazhv@gmail.com>
2024-05-22 14:28:57 +0200

    The adjustemnts to App were made

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Terminal from './Components/Terminal/Terminal';
+import swiss_post_logo1 from "../public/Swiss_Post_logo.png";
+import swiss_post_logo2 from "../public/Schweizerische_Post_Logo.png";
+import swiss_post_plane1 from './../public/swiss-plane1.png';
+import swiss_post_plane3 from '../public/swiss-plane3.png';
+import swiss_post_plane4 from '../public/swiss-plane4.png';
+import swiss_post_plane5 from '../public/swiss-plane5.png';
+import ImageSlider from './Components/Slider/Slider';
+import Concept from './Components/Concepts/Concept';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={swiss_post_logo2} alt='Swiss Post Logo' />
+        <h2>Ready to take off into the world of IT innovation?</h2>
+      </header>
+      <main>
+        <ImageSlider />
+        <div className='name-and-concept'>
+          <Terminal />
+
+          <section className='grid-and-description'>
+            <div className="image-grid">
+              <div className="image">
+                <img src={swiss_post_plane1} alt="Image 1" />
+              </div>
+              <div className="image">
+                <img src={swiss_post_plane4} alt="Image 2" />
+              </div>
+              <div className="image">
+                <img src={swiss_post_plane3} alt="Image 3" />
+              </div>
+              <div className="image">
+                <img src={swiss_post_plane5} alt="Image 4" />
+              </div>
+            </div>
+
+            <div className='description'>
+              <Concept />
+            </div>
+          </section>
+        </div>
+
+      </main>
+      <footer>
+        <img src={swiss_post_logo1} alt='Swiss Post Logo' />
+        <p>&copy; 2024 Swiss Post ICT Academy</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
